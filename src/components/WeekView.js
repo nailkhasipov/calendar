421a4ece6ca9d3bb0f468e73e8638c0165ec8d32
Nail Khasipov <nail.khasipov@gmail.com>
2018-09-28 19:43:10 +0500

    week view

--- src/components/WeekView.js
+++ src/components/WeekView.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 export const WeekView = () => {
 
   return (
-    <div>
-      Week
+    <div class="views view-week">
+      {Array.apply( null, Array( 7 ) ).map( () =>
+        <div class="day-grid">
+          {Array.apply( null, Array( 24 ) ).map( () =>
+            <div class="hour"></div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
