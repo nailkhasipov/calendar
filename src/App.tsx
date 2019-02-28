a6d138de40d58a7ddc947034e422a5125ae15196
Nail Khasipov <nail.khasipov@gmail.com>
2019-02-28 22:33:16 +0500

    setDate&setView hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Views, Navigate } from './types';
 import { Toolbar } from './components/Toolbar';
 
 import 'normalize.css';
 
-export const App = () => (
-  <Toolbar
-    onNavigate={(to: Navigate) => console.log(to)}
-    onChangeView={(view: Views) => console.log(view)}
-  />
-);
+export const App = () => {
+  const [date, setDate] = useState(new Date());
+  const [view, setView] = useState(Views.DAY);
+  const handleNavigate = (to: Navigate) => {};
+  const handleChangeView = (view: Views) => {
+    setView(view);
+  };
+  return (
+    <Toolbar
+      onNavigate={(to: Navigate) => handleNavigate(to)}
+      onChangeView={(view: Views) => handleChangeView(view)}
+    />
+  );
+};
