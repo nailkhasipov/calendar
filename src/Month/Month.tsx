680e17a443702f10086a409eafbe5fc67926a7c1
Nail Khasipov <nail.khasipov@gmail.com>
2019-06-21 14:56:27 +0500

    Update Month.tsx

--- src/Month/Month.tsx
+++ src/Month/Month.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import { VEvent } from '../types';
-import { getMonthArrayWithOffsetAndEvents } from '../utils';
+import {
+  getMonthArrayWithOffsetAndEvents,
+  getMonthNameFromDate,
+  getFullYearFromDate
+} from '../utils';
 
-import { WeekDaysTitles } from './WeekDaysTitles';
+import { MonthWeekLabels } from './MonthWeekLabels';
+import { MonthDay } from './MonthDay';
 
 type MonthProps = {
   date: Date;
@@ -17,61 +22,26 @@ const MonthTable = styled.div`
   height: 100%;
 `;
 
-const MonthTableDay = styled.div`
-  width: 14.2857142857%;
-  height: 16.6666666667%;
-  border-left: 1px solid #eeeeee;
-  border-bottom: 1px solid #eeeeee;
-`;
-
-const MonthTableDayDate = styled.span`
-  width: 100%;
-  text-align: right;
-  display: block;
-  padding: 8px;
-  font-size: 13px;
-`;
-
-const MonthTableDayEvent = styled.div`
-  background: #339af0;
-`;
-
 export const Month = (props: MonthProps) => {
   const monthArray = getMonthArrayWithOffsetAndEvents(
     props.date,
     new Date(),
     true
   );
-  const month_table = monthArray.map((day: any, index) => (
-    <MonthTableDay key={index}>
-      <MonthTableDayDate>{day.date.getDate()}</MonthTableDayDate>
-    </MonthTableDay>
-  ));
-
-  const monthName = props.date.toLocaleString('en-us', { month: 'long' });
-  const fullYear = props.date.getFullYear();
-
   return (
     <div className='cal-view'>
       <div className='cal-view__header'>
         <div className='cal-view__title'>
-          <b>{monthName}</b> {fullYear}
+          <b>{getMonthNameFromDate(props.date)}</b>{' '}
+          {getFullYearFromDate(props.date)}
         </div>
-        <WeekDaysTitles />
+        <MonthWeekLabels />
       </div>
-      <MonthTable>{month_table}</MonthTable>
+      <MonthTable>
+        {monthArray.map((day: any, index) => (
+          <MonthDay key={index} date={day.date} />
+        ))}
+      </MonthTable>
     </div>
-  );
-};
-
-type MonthDayProps = {};
-
-const StyledMonthDay = styled.div``;
-
-const MonthDay = (props: MonthDayProps) => {
-  return (
-    <StyledMonthDay>
-      <MonthTableDayDate>1</MonthTableDayDate>
-    </StyledMonthDay>
   );
 };
