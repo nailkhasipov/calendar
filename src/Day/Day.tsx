81caed80ae96a92505f3856ee3b8b523c215fd51
Nail Khasipov <nail.khasipov@gmail.com>
2019-06-21 19:06:06 +0500

    Day to styled components

--- src/Day/Day.tsx
+++ src/Day/Day.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { VEvent } from '../types';
-import './Day.css';
 import { HoursLabels } from './HoursLabels';
 import { DayGrid } from './DayGrid';
 
@@ -12,28 +11,75 @@ type DayProps = {
   onCreateEvent: (timestamp: number) => void;
 };
 
+const FullCalView = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const FullCalViewHeader = styled.div`
+  font-size: 16px;
+  font-weight: 200;
+  padding-bottom: 8px;
+  border-bottom: 2px solid #eee;
+  margin: 0px 32px 0px 16px;
+  margin-bottom: 4px;
+`;
+
+const FullCalViewHeaderTitle = styled.div`
+  font-size: 24px;
+  font-weight: 500;
+  padding: 16px 16px 8px 16px;
+`;
+
+const FullCalViewHeaderDayGridTitle = styled.div`
+  font-size: 16px;
+  font-weight: 200;
+  padding-bottom: 8px;
+  border-bottom: 2px solid #eee;
+  margin: 0px 32px 0px 16px;
+  margin-bottom: 4px;
+`;
+
+const FullCalViewGridWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  flex-grow: 1;
+`;
+
+const FullCalViewGrid = styled.div`
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  width: 100%;
+  overflow: scroll;
+  height: 100%;
+  padding: 0 32px;
+`;
+
 export const Day = (props: DayProps) => {
   const day = props.date.getDate();
   const monthName = props.date.toLocaleString('en-us', { month: 'long' });
   const fullYear = props.date.getFullYear();
   const dayName = props.date.toLocaleString('en-us', { weekday: 'long' });
   return (
-    <div className='cal-view'>
-      <div className='cal-view__header'>
-        <div className='cal-view__title'>
+    <FullCalView>
+      <FullCalViewHeader>
+        <FullCalViewHeaderTitle>
           <b>
             {day} {monthName}
           </b>{' '}
           {fullYear}
-        </div>
-        <div className='cal-day__grid__header'>{dayName}</div>
-      </div>
-      <div className='cal-day__grid-wrapper'>
-        <div className='cal-day__grid'>
+        </FullCalViewHeaderTitle>
+        <FullCalViewHeaderDayGridTitle>{dayName}</FullCalViewHeaderDayGridTitle>
+      </FullCalViewHeader>
+      <FullCalViewGridWrapper>
+        <FullCalViewGrid>
           <HoursLabels />
           <DayGrid full={true} date={props.date} events={props.events} />
-        </div>
-      </div>
-    </div>
+        </FullCalViewGrid>
+      </FullCalViewGridWrapper>
+    </FullCalView>
   );
 };
