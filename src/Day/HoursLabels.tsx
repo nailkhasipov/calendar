93037c3c264a34e3ba055575b1a124a65d95c2b4
Nail Khasipov <nail.khasipov@gmail.com>
2019-06-21 18:52:53 +0500

    HoursLabels

---
+++ src/Day/HoursLabels.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledHoursLabels = styled.div`
+  margin: 0;
+  padding: 0;
+  left: 0;
+  font-size: 11px;
+  color: #ccc;
+  margin-right: 8px;
+  font-weight: 200;
+`;
+
+const HoursLabel = styled.div`
+  height: 40px;
+`;
+
+export const HoursLabels = () => (
+  <StyledHoursLabels>
+    {[...Array(24)].map((element, index) => (
+      <HoursLabel>{index.toString().padStart(2, '0')}:00</HoursLabel>
+    ))}
+  </StyledHoursLabels>
+);
