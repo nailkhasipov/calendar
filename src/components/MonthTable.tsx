afa4a9d4c7e4211204b6435320c464279729f7d2
Nail Khasipov <nail.khasipov@gmail.com>
2019-06-22 14:36:13 +0500

    MonthTable component

---
+++ src/components/MonthTable.tsx
@@ -0,0 +1,119 @@
+import React from 'react';
+import styled from 'styled-components';
+import {
+  getMonthArray,
+  getDateTitle,
+  isOffsetDay,
+  isSelectedDay,
+  isToday
+} from '../utils';
+
+const today = new Date();
+
+const getDayComponent = (monthArrayDate: Date, selectedDate: Date) => {
+  if (isOffsetDay(monthArrayDate, today))
+    return <OffsetDay>{monthArrayDate.getDate()}</OffsetDay>;
+  if (isSelectedDay(monthArrayDate, selectedDate))
+    return <CurrentDay>{monthArrayDate.getDate()}</CurrentDay>;
+  if (isToday(monthArrayDate)) return <Today>{monthArrayDate.getDate()}</Today>;
+  return <Day>{monthArrayDate.getDate()}</Day>;
+};
+
+type MonthTableProps = {
+  date: Date;
+  onDateChange: (date: Date) => void;
+};
+
+export const MonthTable = (props: MonthTableProps) => {
+  return (
+    <StyledMonthTable>
+      <Title>{getDateTitle(props.date)}</Title>
+      <DayTitles>
+        <DayTitle>M</DayTitle>
+        <DayTitle>T</DayTitle>
+        <DayTitle>W</DayTitle>
+        <DayTitle>T</DayTitle>
+        <DayTitle>F</DayTitle>
+        <DayTitle>S</DayTitle>
+        <DayTitle>S</DayTitle>
+      </DayTitles>
+      <Days>
+        {getMonthArray(props.date).map((monthArrayDate: Date, index) => (
+          <DayWrapper
+            key={index}
+            onClick={() => props.onDateChange(monthArrayDate)}
+          >
+            {getDayComponent(monthArrayDate, props.date)}
+          </DayWrapper>
+        ))}
+      </Days>
+    </StyledMonthTable>
+  );
+};
+
+const StyledMonthTable = styled.div``;
+
+const Title = styled.span`
+  padding: 10px;
+  margin-bottom: 4px;
+`;
+
+const DayTitles = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const DayTitle = styled.div`
+  width: 24px;
+  height: 24px;
+  font-size: 11px;
+  margin: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const DayWrapper = styled.div``;
+
+const Days = styled.div`
+  width: 224px;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+`;
+
+const Day = styled.div`
+  width: 24px;
+  height: 24px;
+  font-size: 11px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  cursor: pointer;
+  margin: 4px;
+  :hover {
+    background: #f1f3f5;
+  }
+`;
+
+const OffsetDay = styled(Day)`
+  color: #eee;
+`;
+
+const CurrentDay = styled(Day)`
+  background: #868e96;
+  color: white;
+  :hover {
+    background: #495057;
+    color: white;
+  }
+`;
+
+const Today = styled(Day)`
+  background: #495057;
+  color: white;
+  :hover {
+    background: #212529;
+  }
+`;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,48 +1,14 @@
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import {
-  getMonthArray,
-  getDateTitle,
-  isOffsetDay,
-  isSelectedDay,
-  isToday
-} from '../utils';
 
-const today = new Date();
-
-const getDayComponent = (monthArrayDate: Date, selectedDate: Date) => {
-  if (isOffsetDay(monthArrayDate, today))
-    return <OffsetDay>{monthArrayDate.getDate()}</OffsetDay>;
-  if (isSelectedDay(monthArrayDate, selectedDate))
-    return <CurrentDay>{monthArrayDate.getDate()}</CurrentDay>;
-  if (isToday(monthArrayDate)) return <Today>{monthArrayDate.getDate()}</Today>;
-  return <Day>{monthArrayDate.getDate()}</Day>;
-};
+import { MonthTable } from './MonthTable';
 
 export const Sidebar: React.FunctionComponent<{
   date: Date;
   onDateChange: (date: Date) => void;
 }> = ({ date, onDateChange }) => (
   <StyledSidebar>
-    <MonthTable>
-      <Title>{getDateTitle(date)}</Title>
-      <DayTitles>
-        <DayTitle>M</DayTitle>
-        <DayTitle>T</DayTitle>
-        <DayTitle>W</DayTitle>
-        <DayTitle>T</DayTitle>
-        <DayTitle>F</DayTitle>
-        <DayTitle>S</DayTitle>
-        <DayTitle>S</DayTitle>
-      </DayTitles>
-      <Days>
-        {getMonthArray(date).map((monthArrayDate: Date, index) => (
-          <DayWrapper key={index} onClick={() => onDateChange(monthArrayDate)}>
-            {getDayComponent(monthArrayDate, date)}
-          </DayWrapper>
-        ))}
-      </Days>
-    </MonthTable>
+    <MonthTable date={date} onDateChange={onDateChange} />
   </StyledSidebar>
 );
 
@@ -52,71 +18,4 @@ const StyledSidebar = styled.div`
   justify-content: center;
   border-right: 1px solid #eee;
   padding: 16px 0;
-`;
-
-const MonthTable = styled.div``;
-
-const Title = styled.span`
-  padding: 10px;
-  margin-bottom: 4px;
-`;
-
-const DayTitles = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-const DayTitle = styled.div`
-  width: 24px;
-  height: 24px;
-  font-size: 11px;
-  margin: 4px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const DayWrapper = styled.div``;
-
-const Days = styled.div`
-  width: 224px;
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
-`;
-
-const Day = styled.div`
-  width: 24px;
-  height: 24px;
-  font-size: 11px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 50%;
-  cursor: pointer;
-  margin: 4px;
-  :hover {
-    background: #f1f3f5;
-  }
-`;
-
-const OffsetDay = styled(Day)`
-  color: #eee;
-`;
-
-const CurrentDay = styled(Day)`
-  background: #868e96;
-  color: white;
-  :hover {
-    background: #495057;
-    color: white;
-  }
-`;
-
-const Today = styled(Day)`
-  background: #495057;
-  color: white;
-  :hover {
-    background: #212529;
-  }
 `;
