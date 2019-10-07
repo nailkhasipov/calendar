f21678554a161d2bd492bde3de729671be6e1adf
Marsel <marsel.kadroff@gmail.com>
2019-10-07 19:45:53 +0500

    render events in day component

--- src/Day/Day.tsx
+++ src/Day/Day.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { VEvent } from "../types";
 import { HoursLabels } from "./HoursLabels";
 import { DayGrid } from "./DayGrid";
+import { DayEvent } from "./DayViewEvent";
 
 import { FullCalViewGrid } from "../styled/FullCal";
 
@@ -14,6 +15,7 @@ export const Day = (props: DayProps) => {
   return (
     <FullCalViewGrid>
       <HoursLabels />
+      <DayEvent date={props.date}></DayEvent>
       <DayGrid full={true} date={props.date} />
     </FullCalViewGrid>
   );
